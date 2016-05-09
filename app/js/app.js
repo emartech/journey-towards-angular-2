cc75fc6aae391f2bd8db60aa34c28db8e0c47f08
Drawain <drawain@gmail.com>
2016-05-09 10:25:37 +0200

    feat(step3-end)

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,10 @@
 import { PhoneRepositoryService } from './services/phone-repository/phone-repository.service';
 import { CarrierRepositoryService } from './services/carrier-repository/carrier-repository.service';
 
+
+import { ShortDescriptionFilter } from './filters/short-description/short-description.filter';
+
+import { PhoneListItemComponent } from './components/phone-list-item/phone-list-item.component';
 import { CopyrightComponent } from './components/copyright/copyright.component';
 
 angular
@@ -15,7 +19,10 @@ angular
   .service('phoneRepository', PhoneRepositoryService.create())
   .service('carrierRepository', CarrierRepositoryService.create())
 
+  .component('phoneListItem', PhoneListItemComponent.create())
   .component('copyright', CopyrightComponent.create())
+
+  .filter('shortDescription', ShortDescriptionFilter.create())
 
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider
@@ -37,4 +44,3 @@ require('./phone-list.ctrl.js');
 require('./phone.ctrl.js');
 require('./carrier-select.directive.js');
 require('./search-box.directive.js');
-require('./short-description.filter.js');
